d5f11d8aaa4f24144ef51da869b30f71f9b8fa56
melodychn <melodyc18113765@tas.tw>
2019-07-19 16:04:08 -0700

    Updates

---
+++ precache-manifest.0bed60aff8a814db4449b42291009324.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfb9fa032cd178c33378ca43debfd5a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f091dc6ee39c3d7222db",
+    "url": "/static/css/main.9d13d590.chunk.css"
+  },
+  {
+    "revision": "f4e05c5e4350be74408d",
+    "url": "/static/js/2.fede29d6.chunk.js"
+  },
+  {
+    "revision": "f091dc6ee39c3d7222db",
+    "url": "/static/js/main.93409113.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "61272c5f0bacc0355c87ca6b10a17391",
+    "url": "/static/media/IMG_2811.61272c5f.jpg"
+  },
+  {
+    "revision": "d41d5598da9512afc3ef8a0019f60489",
+    "url": "/static/media/bruinracing.d41d5598.jpg"
+  },
+  {
+    "revision": "9fed25d260ec9a1afa009d729ed39c1a",
+    "url": "/static/media/chexplain.9fed25d2.gif"
+  },
+  {
+    "revision": "7480fd40e75448b36961d2bb5aef37f9",
+    "url": "/static/media/ieee.7480fd40.gif"
+  },
+  {
+    "revision": "9de18b70c5081f36b8d1ca9b5322ba80",
+    "url": "/static/media/mouse1.9de18b70.jpg"
+  },
+  {
+    "revision": "309307ac78444b220621aafff5196db4",
+    "url": "/static/media/resumewebsite.309307ac.pdf"
+  },
+  {
+    "revision": "32efe70e7aa03f8638cba278859dbdfb",
+    "url": "/static/media/scrolldown.32efe70e.png"
+  }
+]);
